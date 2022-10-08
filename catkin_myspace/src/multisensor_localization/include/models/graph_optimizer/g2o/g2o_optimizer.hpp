b4b90f2964b415602f1445db1047a0b400624ab2
niuwengang <niu_wengang@163.com>
2022-10-08 17:51:03 +0800

    g2o头文件

--- catkin_myspace/src/multisensor_localization/include/models/graph_optimizer/g2o/g2o_optimizer.hpp
+++ catkin_myspace/src/multisensor_localization/include/models/graph_optimizer/g2o/g2o_optimizer.hpp
@@ -1,9 +1,13 @@
-#include "./graph_optimizer_interface.hpp"
+#include "../graph_optimizer_interface.hpp"
 
 #ifndef G2O_OPTIMIZER_HPP_
 #define G2O_OPTIMIZER_HPP_
 
 #include <g2o/core/g2o_core_api.h>
+#include  <g2o/core/factory.h>
+#include <g2o/core/robust_kernel_factory.h>
+#include <g2o/core/optimization_algorithm_factory.h>
+#include <g2o/core/sparse_optimizer.h>
 
 namespace g2o
 {
@@ -20,10 +24,12 @@ namespace multisensor_localization
         /*输入输出数据*/
         virtual bool GetOptimizedPose(std::deque<Eigen::Matrix4f> &Optimized_pose) override;
         virtual bool GetNodeNum() override;
-        /*添加鲁棒核、节点、边、*/
+        /*添加鲁棒核*/
         virtual void SetEdgeRobustKernel(std::string robust_kernel_name, double robust_kernel_size) override;
+        /*添加节点*/
         virtual void AddSe3Node(const Eigen::Isometry3d &pose, bool need_fix) override;
-        //!为什么这里是两个vertex_index???
+        /*添加边*/
+        //!为什么这里是两个vertex_index??? 先验边???
         virtual void AddSe3Edge(int vertex_index1,
                                 int vertex_index2,
                                 const Eigen::Isometry3d &relative_pose,
@@ -35,7 +41,15 @@ namespace multisensor_localization
                                                const Eigen::Quaterniond &quat,
                                                const Eigen::VectorXd noise) override;
         private:
+        /*计算信息矩阵 ps:其实就是权重啦*/
+        Eigen::MatrixXd CalculateSe3EdgeInformationMatrix(Eigen::VectorXd noise);
+        Eigen::MatrixXd CalculateSe3PriorQuaternionEdgeInformationMatrix(Eigen::VectorXd noise);
+        Eigen::MatrixXd CalculateDiagMatrix(Eigen::VectorXd noise);
 
+        g2o::RobustKernelFactory * robust_kernel_factroy_;
+        std::unique_ptr<g2o::SparseOptimizer> graph_ptr_;
+        std::string robust_kernel_size_;
+        bool need_robust_kernel_=false;
     };
 
 } // namespace multisensor_localization
